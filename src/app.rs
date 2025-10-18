cc83f8c96f069e1304a6d741e51df33bef90698c
Bastien VINH <bvinh.dev@yahoo.com>
2025-10-18 20:35:06 +0200

    Router

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,10 @@ use crate::application::Layout;
 use leptos::{ prelude::*};
 use wasm_bindgen::prelude::*;
 
+use leptos_router::components::*;
+use leptos_router::path;
+// use leptos_router::hooks::use_params_map;
+
 #[wasm_bindgen]
 extern "C" {
 	#[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
@@ -11,6 +15,14 @@ extern "C" {
 #[component]
 pub fn App() -> impl IntoView {
 	view! {
-		<Layout />
+		<Router>
+			<Routes fallback=|| "Not found.">
+				<Route path=path!("") view=Layout />
+				<Route path=path!("download") view=|| view! { <h1>"Download Page"</h1> } />
+				<Route path=path!("history") view=|| view! { <h1>"History Page"</h1> } />
+				<Route path=path!("bookmarks") view=|| view! { <h1>"Bookmarks Page"</h1> } />
+				<Route path=path!("settings") view=|| view! { <h1>"Settings Page"</h1> } />
+			</Routes>
+		</Router>
 	}
 }
